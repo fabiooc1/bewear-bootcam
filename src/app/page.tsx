1c0af2b7725453d8571f5bbeabe4aa5757a791b1
fabiooc1 <fabiocaldas266@gmail.com>
2025-08-06 01:04:00 -0300

    feat: product item and listing products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import { Header } from "@/components/common/Header";
+import { Header } from "@/components/common/header";
+import { ProductList } from "@/components/common/product-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true
+    }
+  });
+
   return (
     <>
       <Header />
+
+      <div className="px-5 space-y-6">
+        <Image 
+          src="/banner01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <Image 
+          src="/banner02.png"
+          alt="Autêntico"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 }
